0b22b55528b836232fb21510a0ecc5943a6f8f02
chenchulu <chenchulua@gmail.com>
2018-11-11 16:25:49 +0530

    This is for Jenkins Java Test Job

---
+++ JenkinsJavaTest1/src/com/mytest/FileGenEx.java
@@ -0,0 +1,31 @@
+package com.mytest;
+
+import java.io.FileWriter;
+import java.util.Date;
+
+public class FileGenEx {
+
+	public static void main(String[] args) {
+		
+		
+		  try{    
+	           FileWriter fw=new FileWriter("E:/TempJenkins/File1.txt");    
+	           
+	           fw.write("File modified at 1:13");
+	           fw.write("Welcome to Jenkins-Java Test example");
+	           fw.write("\n");
+	           fw.write("Current time :"+new Date());
+	           
+	           fw.flush();
+	           fw.close();    
+	           fw =null;
+	           
+	          }catch(Exception e){System.out.println(e);}
+		  
+	          System.out.println("File writing Done!");    
+	          
+	          
+
+	}
+
+}
